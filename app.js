09263bc0c1459c2c809b9ec3daec99850c7ffd0b
Mato Bajusic <bajusmat@students.zhaw.ch>
2022-06-03 16:48:18 +0200

    aenderung 1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Cloud Deplyoment Dev Ops Woche 13');
+  res.end('Cloud Deplyoment Dev Ops Woche 13 - Test 1');
 });
 
 server.listen(port, hostname, () => {
