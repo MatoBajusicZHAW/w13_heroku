5e2c37d34cc2de2a971a5cf369deab3c86cdd11f
Mato Bajusic <bajusmat@students.zhaw.ch>
2022-06-03 16:52:58 +0200

    test 2 mato

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Cloud Deplyoment Dev Ops Woche 13 - Test 1');
+  res.end('Cloud Deplyoment Dev Ops Woche 13 - Test 2');
 });
 
 server.listen(port, hostname, () => {
